00fdc2391656ae7c864b65bb3cd9d17659c9d6f3
Mantas <j.mantelijo@gmail.com>
2024-02-06 13:32:37 +0200

    inject version logs

--- cmd/ws-server/main.go
+++ cmd/ws-server/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"d8x-candles/src/svc"
+	"log/slog"
 )
 
+// Injected via -ldflags -X
+var VERSION = "ws-server-development"
+
 func main() {
+	slog.Info("starting service",
+		slog.String("name", "candles-ws-server"),
+		slog.String("version", VERSION),
+	)
 	svc.RunCandleCharts()
 }
