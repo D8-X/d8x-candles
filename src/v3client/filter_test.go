78eef3e74d7a4ab333ce93b5c9ba2818f4f39b0a
b1674927 <b1674927@protonmail.com>
2024-10-31 21:28:44 -0400

    test

--- src/v3client/filter_test.go
+++ src/v3client/filter_test.go
@@ -1,8 +1,11 @@
 package v3client
 
 import (
+	"d8x-candles/env"
 	"fmt"
+	"slices"
 	"testing"
+	"time"
 )
 
 func TestInterpolateTs(t *testing.T) {
@@ -27,5 +30,46 @@ func TestInterpolateTs(t *testing.T) {
 	for j := 0; j < len(blocks); j++ {
 		currobs := obs[blocks[j]]
 		fmt.Printf("%d; %d\n", blocks[j], currobs.ts)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	v := loadEnv()
+	cUni := "../../config/v3_idx_conf.json"
+	cRpc := "../../config/v3_rpc_conf.json"
+	v3, err := NewV3Client(cUni, cRpc, v.GetString(env.REDIS_ADDR), v.GetString(env.REDIS_PW))
+	if err != nil {
+		fmt.Printf("error %v", err)
+		t.FailNow()
+	}
+	nowTs := time.Now().Unix()
+	start := nowTs - LOOKBACK_SEC
+	blk, blkNow, err := v3.findStartingBlock(uint64(start))
+	if err != nil {
+		t.FailNow()
+	}
+	prices, err := v3.runFilterer(int64(blk), int64(blkNow))
+	if err != nil {
+		t.FailNow()
+	}
+	v3.findBlockTs(prices)
+	// now triangulate available prices
+	v3.fillTriangulatedHistory(prices)
+	// check prices
+	blocks := make([]uint64, len(prices))
+	j := 0
+	for block := range prices {
+		blocks[j] = block
+		j++
+	}
+	slices.Sort(blocks)
+	lastTs := uint64(0)
+	for j, block := range blocks {
+		p := prices[block]
+		if p.ts < lastTs {
+			fmt.Printf("wrong order of timestamp at index %d\n", j)
+			t.FailNow()
+		}
+		p.ts = lastTs
 	}
 }
