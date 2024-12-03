4c6dcba28f867572ba9369b2dffd5b14ad7c652d
b1674927 <b1674927@protonmail.com>
2024-12-03 20:53:36 +0100

    align

--- src/utils/lua.go
+++ src/utils/lua.go
@@ -5,17 +5,18 @@ package utils
 // ARGV[1] : fromTs
 // ARGV[2] : toTs
 // ARGV[3] : bucketDur
+// Without ALIGN, bucket start times are multiples of bucketDuration.
 const LUA_OHLC = `
 	local key = KEYS[1]
 	local fromTs = ARGV[1]
 	local toTs = ARGV[2]
 	local bucketDur = ARGV[3]
-
+	
 	local results = {
-		min = redis.call("TS.RANGE", key, fromTs, toTs, "ALIGN", "-", "AGGREGATION", "min", bucketDur),
-		max = redis.call("TS.RANGE", key, fromTs, toTs, "ALIGN", "-", "AGGREGATION", "max", bucketDur),
-		first = redis.call("TS.RANGE", key, fromTs, toTs, "ALIGN", "-", "AGGREGATION", "first", bucketDur),
-		last = redis.call("TS.RANGE", key, fromTs, toTs, "ALIGN", "-", "AGGREGATION", "last", bucketDur),
+		min = redis.call("TS.RANGE", key, fromTs, toTs, "AGGREGATION", "min", bucketDur),
+		max = redis.call("TS.RANGE", key, fromTs, toTs, "AGGREGATION", "max", bucketDur),
+		first = redis.call("TS.RANGE", key, fromTs, toTs, "AGGREGATION", "first", bucketDur),
+		last = redis.call("TS.RANGE", key, fromTs, toTs, "AGGREGATION", "last", bucketDur),
 	}
 	return cjson.encode(results)
 `
